5c0102991a8cb682be07d8c52edd0a23649af2d4
Renan Xavier <renansxdev@gmail.com>
2022-09-01 19:49:51 -0300

    autenticaçao com next-auth utilizando github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/global.scss';
 import { Header} from '../components/Header';
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }) {
   return(
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
